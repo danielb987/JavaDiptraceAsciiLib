c98610208a0fad8927272890168757d956a64ff6
Daniel <db123@bergqvist.se>
2018-04-27 00:28:18 +0200

    Update travis script

--- travis_utility/run_coverage.sh
+++ travis_utility/run_coverage.sh
@@ -13,6 +13,6 @@ if [ "$TRAVIS_REPO_SLUG" == "danielb987/JavaDiptraceAsciiLib" ] && [ "$TRAVIS_PU
   ant -v jacoco_report
 
   # Publish coverage report to https://coveralls.io/github/danielb987/JavaDiptraceAsciiLib
-  mvn coveralls:report
+#  mvn coveralls:report
 
 fi
