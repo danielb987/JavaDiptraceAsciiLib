18688b7b00ef4f494bc28ea6b4f1cd44db8e9cda
Daniel <db123@bergqvist.se>
2018-04-27 00:36:50 +0200

    Update travis script

--- travis_utility/run_ant_and_coverage.sh
+++ travis_utility/run_ant_and_coverage.sh
@@ -7,9 +7,9 @@ set -e
 # travis_utility/push-javadoc-to-docs.sh generates the javadoc and publish it.
 # travis_utility/run_coverage.sh runs the jacoco test, generates the report and and upload it to Coveralls.
 
-exit 3
-
 ant test && \
     travis_utility/run_findbugs.sh && \
     travis_utility/push-javadoc-to-docs.sh && \
     travis_utility/run_coverage.sh
+
+exit 3
