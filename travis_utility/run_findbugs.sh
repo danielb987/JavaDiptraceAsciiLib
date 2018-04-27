153eeca99f366d38977ba5b5f309707e93400ccd
Daniel <db123@bergqvist.se>
2018-04-27 04:38:30 +0200

    Update findbugs

--- travis_utility/run_findbugs.sh
+++ travis_utility/run_findbugs.sh
@@ -9,4 +9,6 @@ export FINDBUGS_HOME=./findbugs/findbugs-3.0.1
 
 mkdir ./build/findbugs
 
-java -jar $FINDBUGS_HOME/lib/findbugs.jar -textui -html -output ./build/findbugs/findbugs.html -sourcepath ./src ./build/classes ./lib
+# java -jar $FINDBUGS_HOME/lib/findbugs.jar -textui -html -output ./build/findbugs/findbugs.html -sourcepath ./src ./build/classes ./lib
+
+java -jar $FINDBUGS_HOME/lib/findbugs.jar -exclude findbugs_exclude.xml -textui -html -output ./build/findbugs/findbugs.html -sourcepath ./src ./build/classes ./lib
