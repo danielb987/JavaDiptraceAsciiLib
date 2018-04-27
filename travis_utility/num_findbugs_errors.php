8525876fb337399f79f0e03ccf90e7f9c398e30e
Daniel <db123@bergqvist.se>
2018-04-27 07:41:16 +0200

    Commit

--- travis_utility/num_findbugs_errors.php
+++ travis_utility/num_findbugs_errors.php
@@ -7,7 +7,7 @@ $file = file_get_contents($argv[1]);
 echo $file;
 
 $pattern = "/\\<b\\>Total Warnings\\<\\/b\\>/ims";
-$pattern = "/Total Warnings/ims";
+// $pattern = "/Total Warnings/ims";
 
 $result = preg_match($pattern, $file, $matches);
 
