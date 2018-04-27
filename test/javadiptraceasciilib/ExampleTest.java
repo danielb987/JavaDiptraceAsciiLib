19be0f11f774c2e89bfa04b8e0386727921e6431
Daniel <db123@bergqvist.se>
2018-04-27 10:01:43 +0200

    Commit

---
+++ test/javadiptraceasciilib/ExampleTest.java
@@ -0,0 +1,23 @@
+package javadiptraceasciilib;
+
+// import util.FindFilesWithNullChar;
+// import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Test the Example class
+ */
+public class ExampleTest {
+    
+    /**
+     * Test the method something.
+     */
+    @Test
+    public void testSomething() {
+        System.out.println("Example");
+        new Example().something();
+//        if (FindFilesWithNullChar.find())
+//            Assert.fail("There is files with null chars");
+    }
+    
+}
