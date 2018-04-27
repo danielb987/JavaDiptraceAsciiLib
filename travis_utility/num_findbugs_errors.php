d25677e08f2f3d3f5c7b0c97001597f07252344e
Daniel <db123@bergqvist.se>
2018-04-27 10:11:48 +0200

    Update travis script

--- travis_utility/num_findbugs_errors.php
+++ travis_utility/num_findbugs_errors.php
@@ -1,19 +1,10 @@
 <?php
 
-echo "Run PHP program. /Daniel.";
-
 $file = file_get_contents($argv[1]);
 
-// echo $file;
-
-// $pattern = "/\\<b\\>Total Warnings\\<\\/b\\>/ims";
 $pattern = "/\\<b\\>Total Warnings\\<\\/b\\>\s+\\<\\/td\\>\s+\\<td align\\=\\\"right\\\"\\>\s+\\<b\\>(\d+)\\<\\/b\\>/ims";
 
 $result = preg_match($pattern, $file, $matches);
-
-// print_r($matches);
-
-// echo "Result: $result\n";
 
 // Return an error code if num total warnings is not zero
 if ($matches[1] != "0") {
