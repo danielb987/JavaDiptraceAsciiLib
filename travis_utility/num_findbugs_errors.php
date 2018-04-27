bcbf39f1f1fec414736abda0f69f3fd8275a57f3
Daniel <db123@bergqvist.se>
2018-04-27 07:13:37 +0200

    Commit

--- travis_utility/num_findbugs_errors.php
+++ travis_utility/num_findbugs_errors.php
@@ -13,9 +13,15 @@ $pattern = '/Total/s';
 
 $result = preg_match($pattern, $file, $matches);
 
+echo "-------------------------------";
+
 print_r($matches);
 
+echo "-------------------------------";
+
 echo "Result: $result\n";
+
+echo "-------------------------------";
 
 preg_match('/(foo)(bar)(baz)/', 'foobarbaz', $matches, PREG_OFFSET_CAPTURE);
 print_r($matches);
