da297c9028fed2ea896cf6d28767897747976566
Daniel <db123@bergqvist.se>
2018-05-01 22:52:40 +0200

    Fix checkstyle errors

--- src/examples/javadiptraceasciilib/FlashLight.java
+++ src/examples/javadiptraceasciilib/FlashLight.java
@@ -33,6 +33,8 @@ public final class FlashLight {
      * @param pcbPartD1 the diode D1 on the pcb
      * @param schematicsPartR1 the resistor R1 on the schematics
      * @param pcbPartR1 the resistor R1 on the pcb
+     * @param newDiodeName the new name of the diode
+     * @param newResistorName the new name of the resistor
      * @param schematicsDistX how long to move the parts on the schematics
      * @param schematicsDistY how long to move the parts on the schematics
      * @param schematicsAngle the new angle on the schematics
