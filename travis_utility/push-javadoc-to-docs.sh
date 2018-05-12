c46cb2a0beabf4e6bffae83c27c1e02877a0e147
Daniel <db123@bergqvist.se>
2018-05-12 20:59:31 +0200

    Update travis script

--- travis_utility/push-javadoc-to-docs.sh
+++ travis_utility/push-javadoc-to-docs.sh
@@ -67,6 +67,7 @@ if [ "$TRAVIS_REPO_SLUG" == "danielb987/JavaDiptraceAsciiLib" ] && [ "$TRAVIS_PU
 
   # Run javadoc and generate package private classes and methods
   cd $DIR
+  rm -Rf dist/javadoc
   ant javadoc_develop
   mv dist/javadoc dist/javadoc_develop
   cd $HOME/temp/JavaDiptraceAsciiLib.Documentation/docs
